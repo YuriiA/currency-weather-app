cd4e162a009e68014065dd82b82e0cfe3ea1a044
Yurii <yurii@MacBook-Pro-2.local>
2023-04-13 19:25:35 +0300

    add onchange event for select and input, state for Exchange rate, if statement to check the input that was changed

--- convertor/src/Components/Convertor.js
+++ convertor/src/Components/Convertor.js
@@ -6,8 +6,20 @@ export default function Convertor() {
   const BASE_URL = "https://api.exchangerate.host/latest";
 
   const [currencySymbol, setCurrencySymbol] = useState([]);
-  const [currencyToConvert, setCurrencyToConvert] = useState([]);
-  const [convertedCurrency, setConvertedCurrency] = useState([]);
+  const [currencyToConvert, setCurrencyToConvert] = useState();
+  const [convertedCurrency, setConvertedCurrency] = useState();
+  const [rate, setRate] = useState();
+  const [amount, setAmount] = useState(1);
+  const [toInput, setToInput] = useState(true);
+
+  let input1, input2;
+  if (toInput) {
+    input2 = amount;
+    input1 = amount * rate;
+  } else {
+    input1 = amount;
+    input2 = amount / rate;
+  }
 
   useEffect(() => {
     fetch(BASE_URL)
@@ -16,20 +28,36 @@ export default function Convertor() {
         setCurrencySymbol([data.base, ...Object.keys(data.rates)]);
         setCurrencyToConvert(data.base);
         setConvertedCurrency(Object.keys(data.rates)[0]);
+        setRate(data.rates[Object.keys(data.rates)[0]]);
       });
   }, []);
 
+  function handleFromChangedAmount(e) {
+    setAmount(e.target.value);
+    setToInput(true);
+  }
+
+  function handleToChangedAmount(e) {
+    setAmount(e.target.value);
+    setToInput(false);
+  }
   return (
     <div className="convertor">
       <h2>Convert</h2>
       <CurrencyInput
         currencySymbol={currencySymbol}
         selectedCurrency={currencyToConvert}
+        changedCurrency={(e) => setCurrencyToConvert(e.target.value)}
+        changedAmount={handleFromChangedAmount}
+        amount={input2}
       />
       <h2>=</h2>
       <CurrencyInput
         currencySymbol={currencySymbol}
         selectedCurrency={convertedCurrency}
+        changedCurrency={(e) => setConvertedCurrency(e.target.value)}
+        changedAmount={handleToChangedAmount}
+        amount={input1}
       />
     </div>
   );
--- convertor/src/Components/CurrencyInput.js
+++ convertor/src/Components/CurrencyInput.js
@@ -1,9 +1,15 @@
 export default function CurrencyInput(props) {
-  const { currencySymbol, selectedCurrency } = props;
+  const {
+    currencySymbol,
+    selectedCurrency,
+    changedCurrency,
+    amount,
+    changedAmount,
+  } = props;
   return (
     <div>
-      <input type="number" />
-      <select value={selectedCurrency}>
+      <input type="number" value={amount} onChange={changedAmount} />
+      <select value={selectedCurrency} onChange={changedCurrency}>
         {currencySymbol.map((option) => (
           <option key={option} value={option}>
             {option}
